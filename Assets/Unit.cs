dbc2dd54df1ce500cb19be500b7ea3b5ab870517
Justin-Lam <justinflam9225k@gmail.com>
2024-01-24 17:02:42 -0800

    Added stuff from the first half of the videoi
    Created Wizard and Enemy prefabs
    Created Unit class and BattleSystem script

---
+++ Assets/Unit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string Unitname;
+    public int maxHP;
+    public int currentHP;
+    //public int Unitlevel;
+    //public int damage;
+}
